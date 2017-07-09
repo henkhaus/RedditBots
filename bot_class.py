b35abb96d3b13969d566f659a77feadc148eb722
rhenk <rhenkhaus@gmail.com>
2017-07-09 17:46:02 -0500

    added RedditBot Class

---
+++ bot_class.py
@@ -0,0 +1,63 @@
+import os
+import praw
+import secrets
+
+
+class RedditBot:
+    # TODO: Need to define method to determine rate limit
+    def __init__(self, name):
+        self.name = name
+        self.r = self.login(name)
+        self.posts_replied_to = self.reply_tracking_unpack(name)
+
+    def login(self, name):
+        secrets.determine_creds(name)
+
+        # connect to reddit and create instance
+        user_agent = 'testing by /u/redditbottesting 0.9.0 for ' + name
+        r = praw.Reddit(user_agent=user_agent,
+                        client_id=secrets.creds.client_id,
+                        client_secret=secrets.creds.client_secret,
+                        username=secrets.creds.username,
+                        password=secrets.creds.password)
+        return r
+
+    def reply_tracking_unpack(self, name):
+        """returns list of posts the bot has already replied to"""
+        filename = name+'_posts_replied_to.txt'
+        if os.path.isfile(filename):
+            # file exists, read contents into list
+            with open(filename, 'r') as f:
+                post_ids = f.read()
+                post_ids = post_ids.split("\n")
+                post_ids = list(filter(None, post_ids))
+        else:
+            # create empty list
+            post_ids = []
+        return post_ids
+
+    def reply_tracking_pack(self, name):
+        """take updated list and write to txt"""
+        filename = name + '_posts_replied_to.txt'
+        with open(filename, 'w') as f:
+            for post_id in self.posts_replied_to:
+                f.write(post_id + '\n')
+
+    def reply_to_comment(self, sub, search_term, reply):
+        """reply to a reddit comment given a subreddit, search term, and reply"""
+        subreddit = self.r.subreddit(sub)
+        for submission in subreddit.hot(limit=5):
+            # see if bot has already replied to the post
+            if submission.id not in self.posts_replied_to:
+                submission.comments.replace_more(limit=0)
+                for comment in submission.comments.list():
+                    if search_term.lower() in comment.body:
+                        comment.reply(reply)
+                        print('Bot replying to : ', submission.title)
+                        # add post id to list
+                        self.posts_replied_to.append(submission.id)
+
+    def kill_bot(self, name):
+        self.reply_tracking_pack(name)
+
+
--- palindrome.py
+++ palindrome.py
@@ -1,11 +1,6 @@
+import reply_format
+import bot_class
 import string
-
-# comment = 'this ia a comment that has the palindrome, racecar.'
-
-
-class RedditBot:
-    def __init__(self, name):
-        self.name = name
 
 
 def comment_parse(s):
@@ -22,7 +17,30 @@ def is_palindrome(s):
     else:
         return False
 
+
+def palindrome_finder(sub, r, posts_replied_to):
+    # TODO: need to add functionality to find multiple palindromes in one comment
+    """reply to a reddit comment given a subreddit, search term, and reply"""
+    subreddit = r.subreddit(sub)
+    for submission in subreddit.hot(limit=10):
+        # see if bot has already replied to the post
+        if submission.id not in posts_replied_to:
+            submission.comments.replace_more(limit=0)
+            for comment in submission.comments.list():
+                for word in comment_parse(comment.body):
+                    if str(is_palindrome(word)) == 'True':
+                        # found palindrome, post reply with formatting
+                        comment.reply(reply_format.add_links(word+'. You made a palindrome!'))
+                        print('Bot replying to : ', submission.title)
+                        # add post id to list
+                        posts_replied_to.append(submission.id)
+
 '''
 for word in comment_parse(comment):
     print(str(is_palindrome(word)))
 '''
+
+if __name__ == '__main__':
+    bot = bot_class.RedditBot('palindrome')
+    palindrome_finder('pythonforengineers', bot.r, bot.posts_replied_to)
+    bot.kill_bot('palindrome')
